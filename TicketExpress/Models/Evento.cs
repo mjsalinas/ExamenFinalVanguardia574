3a3b44ede842931ce40edc18c0de1291e7ef895a
acostaanton2003-afk <anton.acosta@unitec.edu>
2026-09-22 20:22:15 -0600

    ModeloEvento-Create-agrgacion de todos los campos solicitados
    Creacion del ModeloEvento Con las solicitides de los campos y las configuaciones como tal

---
+++ TicketExpress/Models/Evento.cs
@@ -0,0 +1,16 @@
+namespace TicketExpress.Models;
+
+public class Evento
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = string.Empty;
+
+    public string Ciudad { get; set; } = string.Empty;
+
+    public DateTime Fecha { get; set; }
+
+    public int CapacidadTotal { get; set; }
+
+    public decimal PrecioBoleto { get; set; }
+}
