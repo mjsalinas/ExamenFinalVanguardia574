daaf2f6b55efea708909e7e2952296a5585b227f
Heber Pineda <157323289+Heber-Pineda@users.noreply.github.com>
2026-09-22 20:11:16 -0600

    agregen Models  de  Evento y Boleto con propiedades tipadas y relacion uno a muchos mediante EventoId

---
+++ Models/Boleto.cs
@@ -0,0 +1,13 @@
+namespace TicketExpress.Models;
+
+public class Boleto
+{
+    public int Id { get; set; }
+    public int EventoId { get; set; }
+    public string NombreComprador { get; set; } = string.Empty;
+    public string CorreoComprador { get; set; } = string.Empty;
+    public int Cantidad { get; set; }
+    public DateTime FechaCompra { get; set; }
+
+    public Evento? Evento { get; set; }
+}
