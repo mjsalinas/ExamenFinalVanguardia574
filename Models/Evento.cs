b9ef6f1b7b906de8a4592c569f0e5e2284e6ba18
Heber Pineda <157323289+Heber-Pineda@users.noreply.github.com>
2026-09-22 21:06:21 -0600

    Valide el Nombre de Evento en models Evento  ya no queda mucho tiempo solo para espeficicarlo mas

--- Models/Evento.cs
+++ Models/Evento.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TicketExpress.Models;
 
 public class Evento
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [StringLength(100, MinimumLength = 3,
+        ErrorMessage = "El nombre debe tener entre 3 y 100 caracteres.")]
     public string Nombre { get; set; } = string.Empty;
+
     public string Ciudad { get; set; } = string.Empty;
     public DateTime Fecha { get; set; }
     public int CapacidadTotal { get; set; }
