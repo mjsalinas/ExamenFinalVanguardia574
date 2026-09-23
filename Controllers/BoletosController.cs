b843e60884086c171d5009c79cb9d806144fac8e
FabiolaLainez <167152904+FabiolaLainez@users.noreply.github.com>
2026-09-22 20:12:14 -0600

    Creación de controllers -boleto y evento controller

---
+++ Controllers/BoletosController.cs
@@ -0,0 +1,8 @@
+namespace ExamenFinalVanguardia574.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BoletosController : ControllerBase
+{
+    
+}
---
+++ Controllers/EventosController.cs
@@ -0,0 +1,8 @@
+namespace ExamenFinalVanguardia574.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventosController : ControllerBase
+{
+    
+}
