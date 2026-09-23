78918f35987869b03954b2faff1503ce95815446
Milton <leortiz41@gmail.com>
2026-09-22 20:49:48 -0600

    Modelos de Eventos y Boletos Creacions de las entidades o modelos de datos

---
+++ TicketExpress/Models/Boleto.cs
@@ -0,0 +1,15 @@
+namespace TicketExpress.Models
+{
+    public class Boleto
+    {
+        public int Id { get; set; }
+        public int EventoId { get; set; }
+        public string NombreComprador { get; set; }
+
+        public string CorreoComprador { get; set; }
+
+        public int Cantidad { get; set; }
+        public DateTime FechaCompra { get; set; }
+   
+    }
+}
