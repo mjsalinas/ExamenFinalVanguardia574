edf1383148511b4dd26c8a0f2ec56ce7cf7ab497
FabiolaLainez <167152904+FabiolaLainez@users.noreply.github.com>
2026-09-22 20:16:46 -0600

    Se agregó endpoint GetAll en EventoController

--- Controllers/EventosController.cs
+++ Controllers/EventosController.cs
@@ -3,6 +3,16 @@ namespace ExamenFinalVanguardia574.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 public class EventosController : ControllerBase
+
 {
-    
+    private readonly LibraryDbContext _db;
+
+    public EventosController(LibraryDbContext db) => _db = db;
+
+ [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var eventos = await _db.Eventos.ToListAsync();
+        return Ok(eventos);
+    }
 }
