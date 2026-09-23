a3e21298be8f5a1dd913f501e1b9ab183faf2a14
FabiolaLainez <167152904+FabiolaLainez@users.noreply.github.com>
2026-09-22 19:56:36 -0600

    Creación de modelo de datos - boleto.cs y evento.cs

---
+++ Model/Boleto.cs
@@ -0,0 +1,12 @@
+namespace ExamenFinalVanguardia574.Model
+{
+    public class Boleto
+    {
+        public int Id { get; set; }
+        public int EventoId { get; set; }
+        public string NombreComprador { get; set; }
+        public string CorreoComprador { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime FechaCompra { get; set; }
+    }
+}
---
+++ Model/Evento.cs
@@ -0,0 +1,12 @@
+namespace ExamenFinalVanguardia574.Model
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Ciudad { get; set; }
+        public DateTime Fecha { get; set; }
+        public int CapacidadTotal { get; set; }
+        public decimal PrecioBoleto { get; set; }
+    }
+}
