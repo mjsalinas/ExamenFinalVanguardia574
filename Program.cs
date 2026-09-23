ace5fb7884501ea05c5e8f491ad3c74ffe0ffe13
Heber Pineda <157323289+Heber-Pineda@users.noreply.github.com>
2026-09-22 20:36:30 -0600

    configure Swagger con AddSwaggerGen y SwaggerUI para probar endpoints

--- Program.cs
+++ Program.cs
@@ -10,13 +10,15 @@ builder.Services.AddDbContext<TicketExpressDbContext>(options =>
         builder.Configuration.GetConnectionString("DefaultConnection")
         ?? "Data Source=ticketexpress.db"));
 
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
