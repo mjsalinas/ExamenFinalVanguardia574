d83e6914c9f911e2d5089c436b2e7fd7cd363cf4
kofiiRX <kerinzapataaleman@gmail.com>
2026-09-22 20:47:14 -0600

    Creacion del DbContext

---
+++ AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ExamenFinalVanguardia574.Models;
+
+namespace ExamenFinalVanguardia574;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options){}
+
+    public DbSet<Boleto> Boletos => Set<Boleto>();
+    public DbSet<Evento> Eventos => Set<Evento>();
+}
--- Controllers/BoletosController.cs
+++ Controllers/BoletosController.cs
@@ -1,3 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ExamenFinalVanguardia574.Models;
+
+namespace ExamenFinalVanguardia574.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BoletosController : ControllerBase
