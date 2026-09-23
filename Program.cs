411fe29c371e66fc4a46517390406d10ccc1ebf7
Heber Pineda <157323289+Heber-Pineda@users.noreply.github.com>
2026-09-22 20:25:34 -0600

    registre TicketExpressDbContext con UseSqlite y cadena DefaultConnection en appsettings

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using TicketExpress;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddDbContext<TicketExpressDbContext>(options =>
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? "Data Source=ticketexpress.db"));
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
